0c018d9f7e0f7800ac16a16778c3aa8f0fba8f09
Darla Luangrath <darla.luangrath@gmail.com>
2024-08-08 15:31:38 -0700

    Navigate UI

--- src/client/components/About.jsx
+++ src/client/components/About.jsx
@@ -1,11 +1,24 @@
+import Header from './Header'
+import { Outlet, Link } from 'react-router-dom'
+
 const About = () => {
   return (
     <div className='main'>
+      <Header />
+      <div className='subnav'>
+        <Link to='contact' className='subnav_links'>
+          Contact
+        </Link>
+        <Link to='history' className='subnav_links'>
+          History
+        </Link>
+      </div>
       <div className='box'>
-        <h1>About Page</h1>
+        <Outlet />
       </div>
     </div>
   )
 }
 
 export default About
+
--- src/client/components/Home.jsx
+++ src/client/components/Home.jsx
@@ -1,10 +1,21 @@
+import Header from './Header'
+import { Link } from 'react-router-dom'
+
+
 const Home = () => {
   return (
     <div className='main'>
+      <Header />
       <div className='box'>
-        <button className='btn'>Math 1010</button>
-        <button className='btn'>English 2010</button>
-        <button className='btn'>Biology 2020</button>
+        <Link to='classlist/math1010'>
+          <button className='btn'>Math 1010</button>
+        </Link>
+        <Link to='classlist/eng2010'>
+          <button className='btn'>English 2010</button>
+        </Link>
+        <Link to='classlist/bio2020'>
+          <button className='btn'>Biology 2020</button>
+        </Link>
       </div>
     </div>
   )
